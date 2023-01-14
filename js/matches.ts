65d6036a3f2b551e3e0fc7367909d834fa2bd072
hewison-chris <hewisonchris@gmail.com>
2023-01-14 13:43:59 +0900

    Remove alert

--- js/matches.ts
+++ js/matches.ts
@@ -49,9 +49,6 @@ export class Schedule {
           }
           console.log(this.weeks[week].toString())
         }
-        if (week === maxWeeks) {
-          alert("Failed to calculate within 52 weeks due to unlucky randomness. Try again.")
-        }
       })
       if (roundRobinCompleted) {
         matchesCompleted = this.targetMatchCountReached()
